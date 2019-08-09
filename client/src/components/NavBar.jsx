7787acafb5289662ae1ab8945959baed82e44b6d
LadyCrispy <cvg.94@hotmail.com>
2019-08-09 13:16:05 +0200

    responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,16 +10,11 @@ export const NavBar =()=>{
     return(
 
         <Navbar bg="dark" expand="xl" fixed="top">
-          <Navbar.Brand><Link to='/'>FilmFever</Link></Navbar.Brand>
+          <img src="/client/public/icon.jpg" alt="icon"/>
+          <Navbar.Brand><Link to='/' className='nav-links title'>FilmFever</Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav><Link  to='/'>Inicio</Link></Nav>
-              <AddForm/>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
-            </Form>
+              <Nav><Link  to='/' className='nav-links'>Inicio</Link></Nav>
           </Navbar.Collapse>
         </Navbar>
     
