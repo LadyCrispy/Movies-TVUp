dfc9a66ea2edfaebd35606353a97d229a077a240
LadyCrispy <cvg.94@hotmail.com>
2019-08-11 13:51:09 +0200

    Deploying

---
+++ server/public/precache-manifest.c2ad07536fec48f4bc83d3b3100f32be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b6d37136d8b81af5490fc543b7c13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "809e392897039ccf2b33",
+    "url": "/static/css/main.71af0845.chunk.css"
+  },
+  {
+    "revision": "6166e3a1c7d95c328207",
+    "url": "/static/js/2.a9d7c1ce.chunk.js"
+  },
+  {
+    "revision": "809e392897039ccf2b33",
+    "url": "/static/js/main.f557b207.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
